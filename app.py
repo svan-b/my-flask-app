a59c6b161142e8e319dd78b7392214b09c813095
svan-b <scott.vanbolhuis@gmail.com>
2024-01-09 16:08:36 -0800

    Updated home page with responsive CSS

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 @app.route('/')
 def hello_world():
