ca81b88a2c1c6087b37f3d00d24a9c1f3105d64c
svan-b <155944537+svan-b@users.noreply.github.com>
2024-01-21 22:42:03 -0800

    New app pages

--- app.py
+++ app.py
@@ -1,13 +1,37 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def base():
-    return render_template('base.html')
+def index():
+    return render_template('index.html')
 
-# Main execution
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/insights')
+def insights():
+    return render_template('insights.html')
+
+@app.route('/consultation')
+def consultation():
+    return render_template('consultation.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.run(debug=True, port=5001)
+
+
+
 
     
 
