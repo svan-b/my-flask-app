9f11f82e16c982ce14112bb85df173d93087b643
svan-b <155944537+svan-b@users.noreply.github.com>
2024-01-21 22:21:08 -0800

    initial commmit with flask app setup

--- app.py
+++ app.py
@@ -2,16 +2,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
+def base():
+    return render_template('base.html')
 
 # Main execution
 if __name__ == '__main__':
