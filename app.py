59ce1e3452f4ab3cfaf21a26a99e5fec8a30eac8
svan-b <155944537+svan-b@users.noreply.github.com>
2024-01-24 16:00:59 -0800

    updated py app

--- app.py
+++ app.py
@@ -26,6 +26,22 @@ def about():
 def contact():
     return render_template('contact.html')
 
+@app.route('/financial-consulting')
+def financial_consulting():
+    return render_template('financial-consulting.html')
+
+@app.route('/investment-advice')
+def investment_advice():
+    return render_template('investment-advice.html')
+
+@app.route('/market-analysis')
+def market_analysis():
+    return render_template('market-analysis.html')
+
+@app.route('/audio-services')
+def audio_services():
+    return render_template('audio-services.html')
+
 @app.route('/test')
 def test():
     return 'Flask is working!'
@@ -33,11 +49,6 @@ def test():
 if __name__ == '__main__':
     app.run(debug=True, port=5001)  # Runs the application on port 5001
 
-
-
-
-
-    
 
 
 
