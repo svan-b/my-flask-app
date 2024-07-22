cd42677fc9aa7ed81090c8025d6741d9ceb373f3
s-vanb <scott.vanbolhuis@gmail.com>
2024-07-21 23:22:49 -0700

    Fix import error for TextAreaField

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
