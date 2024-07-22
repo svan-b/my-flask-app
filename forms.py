19c3e60d1c7531f15973e423cf9d4aa1f9f0c688
s-vanb <scott.vanbolhuis@gmail.com>
2024-07-21 23:37:07 -0700

    Enhance UI and add additional questions to Coming Soon page

--- forms.py
+++ forms.py
@@ -16,5 +16,8 @@ class LoginForm(FlaskForm):
 class ComingSoonForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     suggestions = TextAreaField('Suggestions', validators=[DataRequired()])
+    question1 = TextAreaField('What feature would you like to see?', validators=[DataRequired()])
+    question2 = TextAreaField('What investment topics are you interested in?', validators=[DataRequired()])
+    question3 = TextAreaField('Any other comments or suggestions?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
